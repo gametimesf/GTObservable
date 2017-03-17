d7f9b52e968ab824806a2ad2e139b6ab32d0d78e
Matt Banach <mbanach@gametime.co>
2017-03-17 10:54:12 -0700

    Create Project

---
+++ GTObservable/GTObservable.h
@@ -0,0 +1,19 @@
+//
+//  GTObservable.h
+//  GTObservable
+//
+//  Created by Matt Banach on 3/17/17.
+//  Copyright © 2017 Gametime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GTObservable.
+FOUNDATION_EXPORT double GTObservableVersionNumber;
+
+//! Project version string for GTObservable.
+FOUNDATION_EXPORT const unsigned char GTObservableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTObservable/PublicHeader.h>
+
+
